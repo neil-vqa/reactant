ef5a8b4758790f241c07e5667ed5d0fd51ae182a
Neil Van <nvq.alino@gmail.com>
2021-09-11 13:24:09 +0800

    add unit tests

--- tests/test_reactant.py
+++ tests/test_reactant.py
@@ -1,10 +1,48 @@
-from reactant import __version__, Reactant, DjangoORM, Field, generate
+from reactant.orm.django import DjangoModel
+from reactant.main import DjangoCombustionChamber
+from reactant import (
+    __version__,
+    Reactant,
+    DjangoORM,
+    Field,
+    generate,
+)
 from typing import Optional
 from pathlib import Path
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_reactant_model_if_a_django_orm_subclass():
+    class RocketEngine(Reactant, DjangoORM):
+        id: int = Field(primary_key=True, title="rocket_id")
+        name: str = Field(max_length=32)
+        manufacturer: str = Field(max_length=64)
+        power_cycle: Optional[str] = Field(
+            "gas-generator", nullable=True, blank=True, max_length=32
+        )
+        thrust_weight_ratio: int
+
+    assert issubclass(RocketEngine, DjangoORM)
+
+
+def test_django_combustion_chamber_get_models_method_return_djangomodels():
+    class RocketEngine(Reactant, DjangoORM):
+        id: int = Field(primary_key=True, title="rocket_id")
+        name: str = Field(max_length=32)
+        manufacturer: str = Field(max_length=64)
+        power_cycle: Optional[str] = Field(
+            "gas-generator", nullable=True, blank=True, max_length=32
+        )
+        thrust_weight_ratio: int
+
+    combust = DjangoCombustionChamber([RocketEngine])
+    models = combust._get_models()
+
+    assert isinstance(models, list)
+    assert isinstance(models[0], DjangoModel)
 
 
 def test_generate_django_files_success():
@@ -17,11 +55,6 @@ def test_generate_django_files_success():
         )
         thrust_weight_ratio: int
 
-    class LaunchVehicle(Reactant, DjangoORM):
-        id: int = Field(primary_key=True)
-        name: str = Field(max_length=32)
-        country: str = Field(blank=True, max_length=32)
-
     generate()
 
     models = Path("models.py")
@@ -33,3 +66,8 @@ def test_generate_django_files_success():
     assert views_class.is_file()
     assert serializers.is_file()
     assert urls_class.is_file()
+
+    models.unlink()
+    views_class.unlink()
+    serializers.unlink()
+    urls_class.unlink()
