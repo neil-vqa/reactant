57cbe401615e50121f145287d6a4719e5b319ca6
Neil Van <nvq.alino@gmail.com>
2021-09-11 12:13:25 +0800

    pla3hce try/except blocks

--- reactant/main.py
+++ reactant/main.py
@@ -34,50 +34,69 @@ class DjangoCombustionChamber:
         return models
 
     def render_manager(self) -> None:
-        models = self._get_models()
-        model_names = [model.name for model in models]
+        try:
+            models = self._get_models()
+            model_names = [model.name for model in models]
 
-        self._render_models(models)
-        self._render_views(model_names)
-        self._render_serializers(models, model_names)
-        self._render_urls(model_names)
+            self._render_models(models)
+            self._render_views(model_names)
+            self._render_serializers(models, model_names)
+            self._render_urls(model_names)
+        except Exception:
+            raise
 
     def _render_models(self, models: List[NamedTuple]) -> None:
-        template_models = env.get_template("django_models.txt.jinja")
-        output_models = template_models.render(models=models)
-        formatted_code = format_str(output_models, mode=FileMode())
-        with open("models.py", "w") as file1:
-            file1.write(formatted_code)
+        try:
+            template_models = env.get_template("django_models.txt.jinja")
+            output_models = template_models.render(models=models)
+            formatted_code = format_str(output_models, mode=FileMode())
+            with open("models.py", "w") as file1:
+                file1.write(formatted_code)
+        except Exception:
+            raise
+        else:
             secho("Django models.py finished rendering.", fg="green")
 
     def _render_views(self, model_names: List[str]) -> None:
-        template_views = env.get_template("django_views.txt.jinja")
-        output_views = template_views.render(names=model_names)
-        formatted_code = format_str(output_views, mode=FileMode())
-        with open("views_class.py", "w") as file2:
-            file2.write(formatted_code)
+        try:
+            template_views = env.get_template("django_views.txt.jinja")
+            output_views = template_views.render(names=model_names)
+            formatted_code = format_str(output_views, mode=FileMode())
+            with open("views_class.py", "w") as file2:
+                file2.write(formatted_code)
+        except Exception:
+            raise
+        else:
             secho("Django views_class.py finished rendering.", fg="green")
 
     def _render_serializers(
         self, models: List[NamedTuple], model_names: List[str]
     ) -> None:
-        template_serializers = env.get_template("django_serializers.txt.jinja")
-        output_serializers = template_serializers.render(
-            models=models, names=model_names
-        )
-        formatted_code = format_str(output_serializers, mode=FileMode())
-        with open("serializers.py", "w") as file3:
-            file3.write(formatted_code)
+        try:
+            template_serializers = env.get_template("django_serializers.txt.jinja")
+            output_serializers = template_serializers.render(
+                models=models, names=model_names
+            )
+            formatted_code = format_str(output_serializers, mode=FileMode())
+            with open("serializers.py", "w") as file3:
+                file3.write(formatted_code)
+        except Exception:
+            raise
+        else:
             secho("Django serializers.py finished rendering.", fg="green")
 
     def _render_urls(self, model_names: List[str]) -> None:
-        snaked_model_names = [convert_to_snake(name) for name in model_names]
-        paired_names = dict(zip(model_names, snaked_model_names))
-        template_urls = env.get_template("django_urls.txt.jinja")
-        output_urls = template_urls.render(names=paired_names)
-        formatted_code = format_str(output_urls, mode=FileMode())
-        with open("urls_class.py", "w") as file3:
-            file3.write(formatted_code)
+        try:
+            snaked_model_names = [convert_to_snake(name) for name in model_names]
+            paired_names = dict(zip(model_names, snaked_model_names))
+            template_urls = env.get_template("django_urls.txt.jinja")
+            output_urls = template_urls.render(names=paired_names)
+            formatted_code = format_str(output_urls, mode=FileMode())
+            with open("urls_class.py", "w") as file3:
+                file3.write(formatted_code)
+        except Exception:
+            raise
+        else:
             secho("Django urls_class.py finished rendering.", fg="green")
 
 
