81582b97d04c7f2a1ee34c594d0130953c63eafe
Neil Van <nvq.alino@gmail.com>
2021-09-28 10:36:06 +0800

    transfer try/except block from runner to generate()

--- reactant/run.py
+++ reactant/run.py
@@ -5,11 +5,5 @@ import click
 @click.command()
 @click.argument("python_file", type=click.Path(exists=True))
 def runner(python_file):
-    try:
-        click.secho(f"Running {python_file}", fg="cyan")
-        subprocess.run(["python", f"{python_file}"])
-    except Exception:
-        click.secho(f"Sorry. Something went wrong.", fg="red")
-        raise
-    else:
-        click.secho(f'Success! Please check "reactant_products" directory.', fg="cyan")
+    click.secho(f"Running {python_file}", fg="cyan")
+    subprocess.run(["python", f"{python_file}"])
